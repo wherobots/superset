ccaee804c8f5261549da4b5d2351e42f8bf08dcf
Pranav Toggi <prantogg@gmail.com>
2024-07-30 19:55:02 -0400

    handle alias name wrapping

--- superset/db_engine_specs/wherobots.py
+++ superset/db_engine_specs/wherobots.py
@@ -1,45 +1,61 @@
-from superset.db_engine_specs import BaseEngineSpec
-import re
-from typing import Any
 import logging
+import re
+from typing import Any, List
 
+from sqlalchemy.engine.reflection import Inspector
+from sqlalchemy.engine.url import URL
+
+from superset.db_engine_specs import BaseEngineSpec
 from superset.models.core import Database
 
 logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger(__name__)
 
+
 class WherobotsEngineSpec(BaseEngineSpec):
-    engine = 'wherobots'
-    engine_name = 'wherobots'
+    engine = "wherobots"
+    engine_name = "wherobots"
     supports_dynamic_schema = True
     supports_catalog = True
 
     @classmethod
-    def adjust_engine_params(cls, uri, connect_args, schema=None, catalog=None):
-        logger.info(
-            f"uses WherobotsEngineSpec - adjust_engine_params() - running...")
+    def adjust_engine_params(  # pylint: disable=unused-argument
+        cls,
+        uri: URL,
+        connect_args: dict[str, Any],
+        catalog: str | None = None,
+        schema: str | None = None,
+    ) -> tuple[URL, dict[str, Any]]:
+        logger.info("uses WherobotsEngineSpec - adjust_engine_params() - running...")
         if schema:
             uri = uri.set(database=schema)
-            connect_args['schema'] = schema
+            connect_args["schema"] = schema
         if catalog:
-            connect_args['catalog'] = catalog
+            connect_args["catalog"] = catalog
         else:
-            connect_args['catalog'] = 'wherobots_open_data'  # Default catalog
+            connect_args["catalog"] = "wherobots_open_data"  # Default catalog
         return uri, connect_args
 
     @classmethod
-    def get_catalog_names(cls, database, schema=None):
-        logger.info(f"uses WherobotsEngineSpec - get_catalog_names() - running...")
-        return ["wherobots", "wherobots_open_data", "wherobots_pro_data"]
+    def get_catalog_names(
+        cls,
+        database: Database,
+        inspector: Inspector,
+    ) -> set[str]:
+        logger.info("uses WherobotsEngineSpec - get_catalog_names() - running...")
+        return {"wherobots", "wherobots_open_data", "wherobots_pro_data"}
 
     @classmethod
-    def get_default_catalog(cls):
-        logger.info(f"uses WherobotsEngineSpec - get_default_catalog() - running...")
+    def get_default_catalog(
+        cls,
+        database: Database,  # pylint: disable=unused-argument
+    ) -> str | None:
+        logger.info("uses WherobotsEngineSpec - get_default_catalog() - running...")
         return "wherobots_open_data"
 
     @classmethod
-    def get_all_catalogs(cls, database):
-        logger.info(f"uses WherobotsEngineSpec - get_all_catalogs() - running...")
+    def get_all_catalogs(cls) -> List[str]:
+        logger.info("uses WherobotsEngineSpec - get_all_catalogs() - running...")
         return ["wherobots", "wherobots_open_data", "wherobots_pro_data"]
 
     @classmethod
@@ -50,52 +66,61 @@ class WherobotsEngineSpec(BaseEngineSpec):
         database: Database,
         **kwargs: Any,
     ) -> None:
-        logger.info(f"uses WherobotsEngineSpec - execute() - running...")
-        """
-        Override the execute method to modify queries before execution.
-        """
-        # Add custom logic to modify the query here
-        # For example, replace single quotes with double quotes in the query
-        query = cls._modify_statement(query)
-        query = cls._sanitize_query(query)
+        logger.info("uses WherobotsEngineSpec - execute() - running...")
+        query = cls._append_catalog_name(query)
+        query = cls._replace_alias_quotes(query)
         # Remove GROUP BY geojson if present
         if "GROUP BY geojson" in query:
-            logger.info(f"uses WherobotsEngineSpec - execute() - modifying query - {query}")
+            logger.info(
+                "uses WherobotsEngineSpec - execute() - modifying query - %s",
+                query,
+            )
             query = query.replace("GROUP BY geojson", "")
-            logger.info(f"uses WherobotsEngineSpec - execute() - modified query - {query}")
+            logger.info(
+                "uses WherobotsEngineSpec - execute() - modified query - %s",
+                query,
+            )
 
         # Call the original execute method with the modified query
         super(WherobotsEngineSpec, cls).execute(cursor, query, database, **kwargs)
 
     @staticmethod
-    def _sanitize_query(query):
-        logger.info(f"uses WherobotsEngineSpec - _sanitize_query() - running...")
-        # Replace problematic alias
-        query = re.sub(r"AS 'COUNT\(\*\)'", "AS count_rows", query)
-        query = re.sub(r'AS "COUNT\(\*\)"', "AS count_rows", query)
-        query = re.sub(r'AS "COUNT\(primary_category\)"', "AS count_primary_category", query)
+    def _replace_alias_quotes(query: str) -> str:
+        """
+        Replace alias names wrapped in double quotes with backticks.
+        """
+        logger.info("uses WherobotsEngineSpec - _replace_alias_quotes() - running...")
+        logger.debug("Original query: %s", query)
+        query = re.sub(r'AS\s+"([^"]+)"', r"AS `\1`", query)
+        logger.debug("Modified query: %s", query)
         return query
 
     @staticmethod
-    def _modify_statement(statement):
-        logger.info(f"uses WherobotsEngineSpec - _modify_statement() - running...")
-        logger.info(f"uses WherobotsEngineSpec - _modify_statement() - statement - {statement}")
+    def _append_catalog_name(statement: str) -> str:
+        logger.info("uses WherobotsEngineSpec - _append_catalog_name() - running...")
+        logger.info(
+            "uses WherobotsEngineSpec - _append_catalog_name() - statement - %s",
+            statement,
+        )
 
-        # Pattern to match FROM, JOIN, and IN with schema.table or schema
-        pattern = re.compile(r'(?P<keyword>FROM|JOIN|IN)\s+(?P<name>\w+(\.\w+)?)', re.IGNORECASE)
+        # Pattern to match FROM, JOIN, and IN followed by a schema.table or schema
+        pattern = re.compile(
+            r"(?P<keyword>FROM|JOIN|IN)\s+(?P<name>\w+(\.\w+)?)", re.IGNORECASE
+        )
 
-        # Find all matches for the pattern
-        matches = pattern.findall(statement)
-        for match in matches:
-            keyword, name, _ = match
-            if not name.startswith('wherobots_open_data.'):
-                if '.' in name:
-                    # If name is in schema.table format
-                    modified_name = f'wherobots_open_data.{name}'
-                else:
-                    # If name is just a schema
-                    modified_name = f'wherobots_open_data.{name}'
-                statement = statement.replace(f'{keyword} {name}', f'{keyword} {modified_name}')
+        # Replace matched patterns with the prefixed schema name
+        modified_statement = re.sub(
+            pattern,
+            lambda m: f'{m.group("keyword")} wherobots_open_data.{m.group("name")}',
+            statement,
+        )
 
-        logger.info(f"uses WherobotsEngineSpec - _modify_statement() - return statement - {statement}")
-        return statement
+        logger.info(
+            "uses WherobotsEngineSpec - _append_catalog_name() - return statement - %s",
+            modified_statement,
+        )
+        return modified_statement
+
+    @classmethod
+    def epoch_to_dttm(cls) -> str:
+        raise NotImplementedError()
